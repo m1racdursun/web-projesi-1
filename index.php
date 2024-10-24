65b2f1e0b85245ee69d10ac3ac4d1219d8b809cc
m1racdursun <dursunmirac2@gmail.com>
2024-10-24 22:28:51 +0300

    ilk dosyalar hazırlandı

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOMBALIK</title>
+</head>
+<body>
+    <h1>TMYO PROJEM</h1>
+    <p>
+        Bu proje ile php bilgimi tazeleyerek yeni projeler oluşturmaya hazırlanacağım.
+
+    </p>
+</body>
+</html>
