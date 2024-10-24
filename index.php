28106f553d51acfeaed5e17f0b94922cb3ce5c37
m1racdursun <dursunmirac2@gmail.com>
2024-10-24 22:44:13 +0300

    index.php dosyasında html şablonu eklendi

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
     <h1>TMYO PROJEM</h1>
     <p>
         Bu proje ile php bilgimi tazeleyerek yeni projeler oluşturmaya hazırlanacağım.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, incidunt vitae? 
+            Neque delectus optio pariatur, vero hic, quasi tempore repellat nam explicabo 
+            illum dicta corrupti, dolorem consequuntur tenetur officia iusto.
 
-    </p>
+        </p>
 </body>
 </html>
